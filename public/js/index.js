a2517e6c3a1b0d068d8ef48bdb8792c64b53dcd3
dygy <yukimurachris@gmail.com>
2019-10-16 19:05:29 +0300

    header footer drop down

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+function hideDropDown() {
+    if (document.getElementById("dropdown").style.display!=="none") {
+        document.getElementById("dropdown").style.display = "none"
+    }
+    else {
+        document.getElementById("dropdown").style.display="flex"
+
+    }
+}
