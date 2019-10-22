a5a5079b8f4ccde06244a6d3d7c490276cf82fdb
dygy <yukimurachris@gmail.com>
2019-10-22 19:47:42 +0300

    mobstand lightspan shtender tables

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+router.get('/tables', function(req, res, next) {
+    res.render('./pages/tables', { title: 'Auth' });
+});
+
+router.get('/shtender', function(req, res, next) {
+    res.render('./pages/shtender', { title: 'Auth' });
+});
+
+router.get('/lightpan', function(req, res, next) {
+    res.render('./pages/lightpan', { title: 'Auth' });
+});
+
+router.get('/mobstand', function(req, res, next) {
+    res.render('./pages/mobstand', { title: 'Auth' });
+});
 router.get('/kron', function(req, res, next) {
     res.render('./pages/kron', { title: 'Auth' });
 });
