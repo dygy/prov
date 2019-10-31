4ee9b146b0c7cd1fdcf3af2170a62e48ccecf6fa
dygy <yukimurachris@gmail.com>
2019-10-31 15:41:41 +0300

    display sells, calculator routers, some style refactorings

--- public/js/Global.js
+++ public/js/Global.js
@@ -1,3 +1,12 @@
 function moveTo(url) {
     location.href = url;
 }
+function displaySell() {
+    document.getElementById("full-page").style.display = "flex";
+    document.getElementsByClassName("sell")[0].style.display = "flex";
+
+}
+function displayProd() {
+    document.getElementById("full-page").style.display = "flex";
+    document.getElementById("window").style.display = "block";
+}
