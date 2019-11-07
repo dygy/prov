93ed8d58872dfac8e25918fa6cd4e6cbd2bf88bc
dygy <yukimurachris@gmail.com>
2019-11-07 18:24:54 +0300

    SPA and some fixes

---
+++ storage.js
@@ -0,0 +1,4 @@
+/*
+in this section will be solution for storing info about products and their cost.
+Most probably it should be 3rd level online storage, as it will be the solution for github deployment system.
+ */
