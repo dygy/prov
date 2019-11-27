9bd3ba7abcb38a490eda2a186d74044f74c82bf5
dygy <yukimurachris@gmail.com>
2019-11-27 14:08:52 +0300

    viv page and full hd fixes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
+router.get('/viv', function(req, res, next) {
+    res.render('./pages/viv', { title: 'Vivesky' });
+});
+
 router.get('/test', function(req, res, next) {
     res.render('./index', { title: 'Auth' });
 });
