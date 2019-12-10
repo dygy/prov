791c82c23ec1703488d304a213d57552b5fd0fa4
dygy <yukimurachris@gmail.com>
2019-12-10 15:22:31 +0300

    img switcher and some fixes, robots.txt

--- public/js/Global.js
+++ public/js/Global.js
@@ -1,17 +1,67 @@
+function elem(id) {
+    return document.getElementById(id)
+}
 function moveTo(url) {
     location.href = url;
 }
 function displaySell() {
-    document.getElementById("full-page").style.display = "flex";
+    elem("full-page").style.display = "flex";
     document.getElementsByClassName("sell")[0].style.display = "flex";
 
 }
 function displayProd() {
-    document.getElementById("full-page").style.display = "flex";
-    document.getElementById("window").style.display = "block";
+    elem("full-page").style.display = "flex";
+    elem("window").style.display = "block";
 }
 function displayImg(url) {
-    document.getElementById("full-page").style.display = "flex";
+    elem("full-page").style.display = "flex";
     document.getElementsByClassName("sell")[1].style.display = "flex";
-    document.getElementById("disImg").src = url;
+    elem("disImg").src = url;
+}
+function switchImg(isNext) {
+    console.log(elem("dot1").src)
+    let imgs = document.getElementsByClassName("smthImg");
+    if (elem("dot1").src.includes("/img/active.png")){
+
+        if (isNext){
+            elem("dot2").src="../img/active.png";
+            elem("dot1").src="../img/inactive.png";
+            imgs[0].style.display="none";
+            imgs[1].style.display="block"
+        }
+        else{
+            elem("dot3").src="../img/active.png";
+            elem("dot1").src="../img/inactive.png";
+            imgs[0].style.display="none";
+            imgs[2].style.display="block"
+        }
+    }
+    else if (elem("dot2").src.includes("/img/active.png")){
+        if (isNext){
+            elem("dot3").src="../img/active.png";
+            elem("dot2").src="../img/inactive.png";
+            imgs[1].style.display="none";
+            imgs[2].style.display="block"
+        }
+        else{
+            elem("dot1").src="../img/active.png";
+            elem("dot2").src="../img/inactive.png";
+            imgs[1].style.display="none";
+            imgs[0].style.display="block"
+        }
+    }
+    else if (elem("dot3").src.includes("/img/active.png")){
+        if (isNext){
+            elem("dot1").src="../img/active.png";
+            elem("dot3").src="../img/inactive.png";
+            imgs[2].style.display="none";
+            imgs[0].style.display="block"
+        }
+        else{
+            elem("dot2").src="../img/active.png";
+            elem("dot3").src="../img/inactive.png";
+            imgs[2].style.display="none";
+            imgs[1].style.display="block"
+        }
+    }
 }
