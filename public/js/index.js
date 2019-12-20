7ee502996d4a98eceee5c4e92120e4d890701154
Dygy <Yukimurachris@gmail.com>
2019-12-20 18:25:18 +0300

    fix

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,15 @@
-function hideDropDown() {
-    if (document.getElementById("dropdown").style.display!=="flex") {
-        document.getElementById("dropdown").style.display = "flex"
+function hideDropDown(id) {
+    if (document.getElementById(id).style.display!=="flex") {
+        document.getElementById("dropdown").style.display = "none";
+        document.getElementById("dropdown2").style.display = "none";
+        document.getElementById("dropdown3").style.display = "none";
+        document.getElementById("dropdown4").style.display = "none";
+        document.getElementById("dropdown5").style.display = "none";
+        document.getElementById(id).style.display = "flex"
+
     }
     else {
-        document.getElementById("dropdown").style.display="none"
+        document.getElementById(id).style.display="none"
 
     }
 }
