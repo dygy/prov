ad0dca8c19eb84e3b7c50db62654602d53222b70
dygy <yukimurachris@gmail.com>
2020-01-16 12:56:36 +0300

    ordering, fixes

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,12 @@
 window.onscroll = function(ev) {
-    if (((window.innerHeight + window.scrollY) >= 1000)) {
+    console.log(window.innerWidth + ' '+ window.innerHeight + window.scrollY)
+    if (((window.innerHeight + window.scrollY) >= window.innerWidth/1.5)) {
         document.getElementById("header").style.display="none";
         document.getElementById("header2").style.display="block";
         hideDropDown("dropdown")
         hideDropDown("dropdown")
     }
-    else if (((window.innerHeight + window.scrollY) <= 1000)) {
+    else if (((window.innerHeight + window.scrollY) <= window.innerWidth)) {
         document.getElementById("header").style.display="block"
         document.getElementById("header2").style.display="none";
 
